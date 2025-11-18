198919f4b8828b909b903ce65d3e57212571350d
nicolaschica <andreshicacordobaa@gmail.com>
2025-11-18 08:27:49 -0500

    Primera versión, solo suma y resta

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+def sumar(num1, num2):
+    return num1 + num2
+
+def restar(num1, num2):
+    return num1 - num2
+
+print("\nOperaciones disponibles:")
+print("1. Sumar")
+print("2. Restar")
+
+opcion = input("\nElige una operación (1-2): ")
+
+print()
+if opcion == "1":
+    print(f"Resultado: {sumar(num1, num2)}")
+elif opcion == "2":
+    print(f"Resultado: {restar(num1, num2)}")
+else:
+    print("Opción inválida")
